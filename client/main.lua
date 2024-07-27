5c4aef88d94730aae7b0f77ee6127c754e12b59a
Mustache Dom <123716453+Mustachedom@users.noreply.github.com>
2024-07-26 23:07:02 -0700

    Add files via upload

--- client/main.lua
+++ client/main.lua
@@ -1,21 +1,40 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 local isUIOpen = false
 
+CreateThread(function()
+for k, v in pairs (Config.Locations) do 
+    if v.job == nil then return end
+    print(v.job)
+    local options = {
+        {
+            label = 'Open Boss Menu',
+            event = 'qb-bossmenu:client:OpenMenu',
+            canInteract = function()
+                if QBCore.Functions.GetPlayerData().job.name == v.job then return true end
+            end
+        }
+    }
+    if GetResourceState('qb-target') == 'started' then
+        exports['qb-target']:AddBoxZone('mdbossmenus'..k, v.loc, 1.5, 1.75, {name = 'mdbossmenus'..k,minZ = v.loc.z-2,maxZ = v.loc.z+2,}, {options = options, distance = 2.0})
+        print('QB-TARGET')
+    elseif GetResourceState('ox_target') == 'started' then
+        bossmenu = exports.ox_target:addBoxZone({ coords = v.loc, size = vec(1,1,2), rotation = 0, debug = false, options = options})
+    end
+end
+end)
 
 local function OpenUI()
     local Player = QBCore.Functions.GetPlayerData()
-    if Player.job.isboss then
-        if not isUIOpen then
-            TriggerEvent('animations:client:EmoteCommandStart', {'tablet'}) 
-            isUIOpen = true
-            SetNuiFocus(true, true)
-            TriggerServerEvent('bossmenu:server:GetEmployees')
-            SendNUIMessage({
-                action = "openUI"
-            })
-        end
-    else
-        Notify("You are not authorized to access the boss menu.", "error")
+    if not isUIOpen then
+        TriggerEvent('animations:client:EmoteCommandStart', {'tablet'}) 
+        isUIOpen = true
+        SetNuiFocus(true, true)
+        TriggerServerEvent('bossmenu:server:GetEmployees')
+        SendNUIMessage({
+            action = "openUI",
+            isBoss = Player.job.isboss,
+            menuItems = Config.MenuItems
+        })
     end
 end
 
@@ -52,7 +71,7 @@ end)
 
 RegisterNUICallback('fireEmployee', function(data, cb)
     local check, name =  lib.callback.await('md-bossmenu:server:fire', false, data.employeeId)
-    if check ~= true then CloseUI() return end
+    if check ~= true then CloseUI()  end
     cb('ok')
 end)
 
@@ -61,3 +80,21 @@ exports('OpenBossMenu', OpenUI)
 RegisterCommand('openbossmenu', function()
     OpenUI()
 end, false)
+
+
+RegisterNUICallback('payBonus', function(data, cb)
+    local check, name, amount, account = lib.callback.await('md-bossmenu:server:PayBonus', false, data.amount, data.employeeId)
+    if check then 
+         Notify('You Paid ' .. amount .. ' To ' .. name .. '!', 'success')
+    end
+     cb('ok')
+ end)
+
+ RegisterNetEvent('md-bossmenu:client:Result', function(type, biz, val)
+    print(type, biz, val)
+    if type == 'fired' then
+        Notify('You Have Been Fired From ' .. biz .. '!', 'error')
+    elseif type == 'paid' then 
+        Notify('You Have Recieved A Bonus of $' .. val .. ' From ' .. biz .. '!', 'success')
+    end
+end)
--- config.lua
+++ config.lua
@@ -1,2 +1,15 @@
 Config = {}
 Config.Notify = 'qb' -- qb/ox/okok
+
+Config.MenuItems = {
+    { icon = 'home', bossOnly = false }, -- home page
+    { icon = 'users', bossOnly = true }, -- employees page
+    { icon = 'chart-bar', bossOnly = true }, -- stats page
+    { icon = 'gift', bossOnly = true }, -- bonuses page
+    { icon = 'cog', bossOnly = false } -- settings
+}
+
+Config.Locations = {
+    {loc = vector3(93.02, -1291.14, 29.33), job = 'police'}
+
+}
